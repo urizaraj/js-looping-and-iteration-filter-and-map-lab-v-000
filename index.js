f241c595da0d6d38c2a4d0623148d765378e455b
Alexander Urizar <urizaraj@gmail.com>
2018-03-29 17:56:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here:
+function driversWithRevenueOver(drivers) {
+
+}
+
+function driverNamesWithRevenueOver(drivers) {
+
+}
+
+function exactMatch(drivers) {
+
+}
+
+function exactMatchToList(drivers) {
+
+}
